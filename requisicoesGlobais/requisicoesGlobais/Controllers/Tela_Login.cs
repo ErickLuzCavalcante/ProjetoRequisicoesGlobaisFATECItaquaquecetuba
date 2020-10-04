5e8f7ec627aba58ee3cdd8b2385295b5e0c57cc6
Samuel Sales <samuelsales81@gmail.com>
2020-10-03 22:57:03 -0300

    Criado Estrutura Login

---
+++ requisicoesGlobais/requisicoesGlobais/Controllers/Tela_Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace requisicoesGlobais.Controllers
+{
+    // Back-end da pagina de login 
+    public class Tela_Login
+    {
+    }
+}
