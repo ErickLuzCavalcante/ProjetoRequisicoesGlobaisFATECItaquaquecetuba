975a8a8dbdb1812af9b8cfd88662a9454818a331
Samuel Sales <samuelsales81@gmail.com>
2020-10-03 23:17:43 -0300

    Ajuste Login

--- requisicoesGlobais/requisicoesGlobais/Controllers/Tela_Login.cs
+++ requisicoesGlobais/requisicoesGlobais/Controllers/Tela_Login.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace requisicoesGlobais.Controllers
 {
-    // Back-end da pagina de login 
-    public class Tela_Login
+    public class Tela_Login : ApiController
     {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
     }
 }
