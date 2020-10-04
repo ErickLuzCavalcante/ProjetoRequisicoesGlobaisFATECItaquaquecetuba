5379f48f586106adb7f315db610493ba6f925ad0
Samuel Sales <samuelsales81@gmail.com>
2020-10-03 22:12:59 -0300

    Classes Alunos

---
+++ requisicoesGlobais/requisicoesGlobais/Classes/Alunos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+ 
+namespace requisicoesGlobais.Classes
+{
+    public class Alunos <extend>
+    {
+        // Atributos com geters e seters 
+        public String id_aluno { get; set; }
+        public String ra_aluno { get; set; }
+        public String turno_aluno { get; set; }
+        public String id_curso { get; set; }
+        public String id_usuario { get; set; }
+      
+        // Metodos 
+        public void cadastrar_aluno()
+        {
+
+        }
+        public void alterar_aluno()
+        {
+
+        }
+        public void consultar_aluno()
+        {
+
+        }
+        public Boolean existe_aluno()
+        {
+            return true;
+        }
+    }
+    
+}
