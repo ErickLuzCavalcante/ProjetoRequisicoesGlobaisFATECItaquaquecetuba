e49a0e01274aaad694b25b93872af13a3cf07588
Samuel Sales <samuelsales81@gmail.com>
2020-10-11 22:10:28 -0300

    Alteração da rota para iniciar na pagina de Login

--- requisicoesGlobais/requisicoesGlobais/App_Start/RouteConfig.cs
+++ requisicoesGlobais/requisicoesGlobais/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace requisicoesGlobais
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
