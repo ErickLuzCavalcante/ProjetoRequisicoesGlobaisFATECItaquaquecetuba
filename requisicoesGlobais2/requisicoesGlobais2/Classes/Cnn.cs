ad3834db311f2e524bd75c380d31051512ae4181
Erick Cavalcante <erickl.cavalcante@gmail.com>
2020-11-22 16:25:45 -0300

    Voltado com o sctipt SQL deletado

--- requisicoesGlobais2/requisicoesGlobais2/Classes/Cnn.cs
+++ requisicoesGlobais2/requisicoesGlobais2/Classes/Cnn.cs
@@ -42,9 +42,34 @@ namespace requisicoesGlobais2.Classes
             }
         }
 
-        public void Saida()
+        public  String Saida(String ComandoSQL)
         {
-
+            String Volta = "erro";
+            try
+            {
+                // Cria um objeto de conexao com o banco de dados
+                SqlConnection conn = new SqlConnection(this.stringConexao);
+                // envia o comando
+                SqlCommand comando = new SqlCommand(ComandoSQL, conn);
+                //abre a conexao
+                conn.Open();
+                //executa o comando com os parametros que foram adicionados acima
+                using (SqlDataReader reader = comando.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        Volta=(String.Format("{0}", reader["Id_curso"]));
+                    }
+                }
+                //fecha a conexao
+                conn.Close();
+                
+            }
+            catch (Exception e)
+            {
+                
+            }
+            return Volta;
         }
     }
 }
